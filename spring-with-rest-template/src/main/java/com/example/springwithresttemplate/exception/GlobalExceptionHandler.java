31e5f2a242d60c02951cdb41b0916dc2e7d48f47
suchit727 <suchitkhadtar12@gmail.com>
2022-12-14 11:24:41 +0530

    Added rest with eureaka

---
+++ spring-with-rest-template/src/main/java/com/example/springwithresttemplate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.example.springwithresttemplate.exception;
+
+import org.springframework.core.ExceptionDepthComparator;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.URI;
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler  {
+
+    @ExceptionHandler(ConnectException.class)
+    public ResponseEntity<?> globalException(HandleExce exce, WebRequest webRequest) {
+        exce.setMessage("this is 500");
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exce.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<?> globalException(HttpServerErrorException httpServerErrorException, WebRequest webRequest) {
+//        exce.setMessage("this is 500");
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), httpServerErrorException.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorDetails>  illegalStateException(IllegalStateException illegalStateException,WebRequest webRequest) {
+//     HandleExce handleExce=new HandleExce("This error is interesting");
+     ErrorDetails errorDetails = new ErrorDetails(new Date(),"This error is interesting" , webRequest.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDetails> notFoundException(NotFoundException notFoundException,WebRequest webRequest){
+        return new ResponseEntity(new ErrorDetails(new Date(),"Not Found" , webRequest.getDescription(false)),HttpStatus.NOT_FOUND);
+    }
+
+}
